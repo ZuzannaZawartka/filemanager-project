ccbc10f3416052834ec72db372959c3caab38376
Zuzia <raf.zuzia7@gmail.com>
2022-01-17 22:42:14 +0100

    added particle

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express")
 const app = express()
-const PORT = process.env.PORT || 5000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
+const PORT = process.env.PORT || 3000; // bardzo istotna linijka - port zostaje przydzielony przez Heroku
 const path = require('path')
 var formidable = require('formidable');
 app.use(express.static('static'))
 let hbs = require('express-handlebars');
 const { count } = require("console");
+const { mainModule } = require("process");
 app.set('views', path.join(__dirname, 'views'));         // ustalamy katalog views
 app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));   // domyślny layout, potem można go zmienić
+app.engine('hbs', hbs({
+    extname: '.hbs',
+    partialsDir: "views/partials",
+}));
+
 app.set('view engine', 'hbs');
 let data = []
 let filesToCount = -1;
@@ -17,6 +23,8 @@ app.listen(PORT, function () {
     console.log("start serwera na porcie " + PORT)
 })
 
+
+
 app.get("/", function (req, res) {
     res.render('upload.hbs');
 });
@@ -25,25 +33,61 @@ app.get("/filemanager", function (req, res) {
     res.render('filemanager.hbs', { data });
 });
 
-app.get("/info/:id", function (req, res) {
-    res.render('info.hbs');
+app.get("/info", function (req, res) {
+    // let fileId = req.query.id
+
+    let file = [];
+    let filePlace = 0;
+    if (data.length > 0) {
+        data.forEach(element => {
+
+            if (element.id == req.query.id) {
+                file.push(data[filePlace])
+                //app.engine('hbs', hbs({ defaultLayout: 'main.hbs' }));
+                //res.render('info.hbs', { file })
+                // return
+            }
+            filePlace++
+        });
+    }
+
+    res.render('info.hbs', { file })
+
 });
 
+
 app.get("/delete", function (req, res) {
+    let count = 0;
     data.forEach(element => {
 
         if (element.id == req.query.id) {
-            data.splice((element.id), 1)
+            data.splice((count), 1)
+
         }
+        count++
     });
-
-
-
-    console.log(req.query.id)
-    console.log(data)
-
     res.redirect('/filemanager')
+});
 
+
+app.get("/reset", function (req, res) {
+    data = []
+    res.redirect('/filemanager')
+});
+
+app.get("/download/", function (req, res) {
+
+    let file = [];
+    let filePlace = 0;
+    data.forEach(element => {
+
+        if (element.id == req.query.id) {
+            //  res.download(data[filePlace].path, data[filePlace].name)
+            res.download(data[req.query.id].path, data[req.query.id].name)
+            console.log(data[req.query.id].path)
+        }
+        filePlace++
+    });
 
 });
 
@@ -58,8 +102,9 @@ app.post('/fileUpload', function (req, res) {
 
         let filesUploaded = files.imagetoupload
         res.redirect('/filemanager')
+        console.log(req.body)
 
-
+        console.log(files)
         if (Array.isArray(filesUploaded)) {
             filesUploaded.forEach(element => {
 
@@ -68,9 +113,11 @@ app.post('/fileUpload', function (req, res) {
                 let img = element.name
                 let name = element.name
                 let size = element.size
+                let path = element.path
+                let savedate = element.lastModifiedDate.getTime()
                 let type = element.type
-                data.push({ id, name, size, type })
-                console.table(data)
+                data.push({ id, name, size, type, path, savedate })
+
 
             });
         } else {
@@ -80,8 +127,10 @@ app.post('/fileUpload', function (req, res) {
             let img = filesUploaded.name
             let name = filesUploaded.name
             let size = filesUploaded.size
+            let path = filesUploaded.path
+            let savedate = filesUploaded.lastModifiedDate.getTime()
             let type = filesUploaded.type
-            data.push({ id, name, size, type })
+            data.push({ id, name, size, type, path, savedate })
         }
 
 
@@ -89,5 +138,6 @@ app.post('/fileUpload', function (req, res) {
 
 
     });
+
 
 });
